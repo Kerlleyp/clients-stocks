c05cdad7668b784a7a3f9e6140f10f2491ae292f
Kerlley Pacheco <kerlleypachecol@gmail.com>
2026-08-12 19:14:10 -0300

    Colocando o site na internet

--- db/conexao.php
+++ db/conexao.php
@@ -1,8 +1,8 @@
 <?php
-$host = "localhost";
-$dbname = "client-stock";
-$user = "root";
-$pass = "";
+$host = "sql113.infinityfree.com";
+$dbname = "if0_42633501_clientstock";
+$user = "if0_42633501";
+$pass = "IntTK2FrpY6h";
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
